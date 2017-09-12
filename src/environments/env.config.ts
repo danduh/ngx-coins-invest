006c54868a1b770a6c1e688b3b1b295bf459f8d9
Daniel Ostrovsky <danduh@gmail.com>
2017-09-12 18:06:36 +0300

    deployment proccec minor changes

--- src/environments/env.config.ts
+++ src/environments/env.config.ts
@@ -1,6 +1,6 @@
 export const allConfig = {
     prod: {
-        baseApiUrl: 'https://i10kcmw72h.execute-api.us-east-1.amazonaws.com/dev/',
+        baseApiUrl: 'https://api-dev.danduh.me/v1/',
         marketTickerUrl: 'https://api.coinmarketcap.com/v1/ticker/',
         name: 'production'
     },
