22a01530bfa9cb201c89b20572c08a2679f46366
Daniel Ostrovsky <danielo@stuccomedia.com>
2017-10-25 22:04:02 +0300

    polonex

--- src/app/services/external-api/poloniex-wss.service.ts
+++ src/app/services/external-api/poloniex-wss.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@angular/core';
 import { WebsocketService } from './websocket.service';
 import { Subject } from 'rxjs/Subject';
 import * as autobahn from 'autobahn';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { CommonTickerService } from './common-ticker.service';
 
 @Injectable()
-export class PoloniexWssService extends WebsocketService {
+export class PoloniexWssService extends CommonTickerService {
+    private subject: Subject<MessageEvent>;
     private wsuri = "wss://api.poloniex.com/";
     public tiks: Subject<any>;
     connection;
@@ -12,6 +16,10 @@ export class PoloniexWssService extends WebsocketService {
     constructor() {
         super();
         this.init();
+    }
+
+    setPairs(pairs) {
+        this.addPairTick(pairs);
     }
 
     init() {
@@ -25,6 +33,7 @@ export class PoloniexWssService extends WebsocketService {
             }
 
             function tickerEvent(args, kwargs) {
+                this.emitPairTick(args);
                 console.log(args);
             }
 
@@ -32,16 +41,44 @@ export class PoloniexWssService extends WebsocketService {
                 // console.log(args);
             }
 
-            session.subscribe('BTC_XMR', marketEvent);
+            // session.subscribe('BTC_XMR', marketEvent);
             session.subscribe('ticker', tickerEvent);
-            session.subscribe('trollbox', trollboxEvent);
-        }
+            // session.subscribe('trollbox', trollboxEvent);
+        };
 
         this.connection.onclose = function () {
             console.log("Websocket connection closed");
-        }
+        };
 
         this.connection.open();
+    }
+
+    public connect(url): Subject<MessageEvent> {
+        if (!this.subject) {
+            this.subject = this.create(url);
+            console.log("Successfully connected: " + url);
+        }
+        return this.subject;
+    }
+
+    private create(url): Subject<MessageEvent> {
+        let ws = new WebSocket(url);
+
+        let observable = Observable.create(
+            (obs: Observer<MessageEvent>) => {
+                ws.onmessage = obs.next.bind(obs);
+                ws.onerror = obs.error.bind(obs);
+                ws.onclose = obs.complete.bind(obs);
+                return ws.close.bind(ws);
+            });
+        let observer = {
+            next: (data: Object) => {
+                if (ws.readyState === WebSocket.OPEN) {
+                    ws.send(JSON.stringify(data));
+                }
+            }
+        }
+        return Subject.create(observer, observable);
     }
 
     _init() {
